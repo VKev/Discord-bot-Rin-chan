c556f3fde23ebd9255d4f34494031e74213039e7
VKev <huynhkhang7452@gmail.com>
2024-04-29 00:39:17 +0700

    up

--- rin.py
+++ rin.py
@@ -1,5 +1,6 @@
 import discord 
 from discord.ext  import commands, tasks
+from discord import app_commands
 from itertools import cycle
 import os
 import asyncio
@@ -20,8 +21,23 @@ async def change_status():
 async def on_ready():
     print(f'{client.user} has wake up !')
     change_status.start()
+    try:
+        synced = await client.tree.sync()
+        print(f"Successfully synced {len(synced)} commands.")
+    except Exception as e:
+        print(e)
 
 
+
+
+@client.tree.command(name="hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hello {interaction.user.mention}", ephemeral=True)
+
+@client.tree.command(name="say")
+@app_commands.describe(thing_to_say= "What should I say?")
+async def say(interaction: discord.Interaction, thing_to_say: str):
+    await interaction.response.send_message(f"{interaction.user.name} said: {thing_to_say}", ephemeral=True)
 
 @client.event
 async def on_command_error(ctx,error):
